a5f13ca78f31960a6f23802783843a47ba883bcb
Rushikesh Teli <Rushikesh.Teli@au.abnamroclearing.com>
2019-02-25 10:37:24 +1100

    starter-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = 'Test';
-  users = ['Test', 'AutoTest'];
-  showDetails = false;
-  clickCounter = 0;
-
-  resetUsername() {
-    this.username = '';
-  }
-
-  addUser() {
-    this.users.push(this.username);
-  }
-
-  showReset() {
-    if (this.username === '') {
-      return false;
-    }
-    return true;
-  }
-
-  toggleDetails() {
-    this.showDetails = !this.showDetails;
-    this.clickCounter++;
-  }
-
-  displayClass() {
-    return this.clickCounter >= 5 ? true : false;
-  }
-
-  getBackground() {
-    return this.clickCounter >= 5 ? 'blue' : 'white';
-  }
 }
