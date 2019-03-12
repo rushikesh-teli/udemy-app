0429559fb8140c3f38f48b2254184797b883de22
Rushikesh Teli <Rushikesh.Teli@au.abnamroclearing.com>
2019-03-12 14:36:57 +1100

    component-databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
-  onIntervalFired(firedNumber: number) {
-    //console.log('Emitted' + firedNumber);
-    if ( firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    } else {
-      this.oddNumbers.push(firedNumber);
-    }
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
   }
 }
